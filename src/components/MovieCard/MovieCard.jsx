079436c57b31b0253409252fbbbb797a3ec12794
GABRIEL DE SOUZA FURTADO <121953504+gabrielsousaf@users.noreply.github.com>
2023-07-31 21:38:14 -0300

    Complete Top Rated

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,39 +1,42 @@
 import { Link } from "react-router-dom";
-import {FaStar}  from "react-icons/fa";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import {FaStar, FaPlay}  from "react-icons/fa";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
 
 const imageUrl = import.meta.env.VITE_IMG;
-const ApiKey = import.meta.env.VITE_API_KEY;
-const MovieEndpoint = import.meta.env.VITE_API;
+// const ApiKey = import.meta.env.VITE_API_KEY;
+// const MovieEndpoint = import.meta.env.VITE_API;
 
 
 const MovieCard = ({ movie, showLink = true }) => {
-  const [ trailerLink, setTrailerLink ] = useState(null);
+  // const [ trailerLink, setTrailerLink ] = useState(null);
 
-  useEffect(() => {
-    axios.get(`${MovieEndpoint}/${movie.id}?${ApiKey}&append_to_response=videos`)
-      .then(response => {
-        if (response.data.videos?.results.length > 0) {
-          const trailerKey = response.data.videos.results[0].key;
-          setTrailerLink(`https://www.youtube.com/watch?v=${trailerKey}`);
-        }
-      })
-      .catch(error => {
-        console.error('Erro ao buscar detalhes do filme:', error)
-      });
-  },[movie.id]);
+  // useEffect(() => {
+  //   axios.get(`${MovieEndpoint}/${movie.id}?${ApiKey}&append_to_response=videos`)
+  //     .then(response => {
+  //       if (response.data.videos?.results.length > 0) {
+  //         const trailerKey = response.data.videos.results[0].key;
+  //         setTrailerLink(`https://www.youtube.com/watch?v=${trailerKey}`);
+  //       }
+  //     })
+  //     .catch(error => {
+  //       console.error('Erro ao buscar detalhes do filme:', error)
+  //     });
+  // },[movie.id]);
   
   return (
     <div className="movie-card">
-      <img src={imageUrl + movie.poster_path} alt={movie.title} />
-      <p> <FaStar /> {movie.vote_average} </p>
-      <h2>{movie.title}</h2>
-      <div className="teste">
-        {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
-        {trailerLink && <a href={trailerLink} target="_blank" rel="noopener noreferrer">Assistir Trailer</a>}
+      {showLink && <Link className="details" to={`/movie/${movie.id}`}>
+        <img src={imageUrl + movie.poster_path} alt={movie.title} />
+        <h2>{movie.title}</h2>
+      </Link>}
 
-      </div>
+      {/* <p> <FaStar /> {movie.vote_average} </p> */}
+ 
+
+      {/* {trailerLink && <a href={trailerLink} target="_blank" className="trailer" rel="noopener noreferrer"> <FaPlay/> <span> Assistir Trailer </span></a>}
+      {showLink && <Link className="details" to={`/movie/${movie.id}`}>Detalhes</Link>} */}
+
     </div>
   )
 }
